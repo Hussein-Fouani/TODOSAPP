cd311f217d86e2ff3fe133162f0f5f52da88a4b3
Hussein <social.101.acc@gmail.com>
2023-09-07 15:58:21 +0300

    Adding dependencies to the project

---
+++ src/main/java/com/example/todosapp/TodosappApplication.java
@@ -0,0 +1,13 @@
+package com.example.todosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodosappApplication.class, args);
+    }
+
+}
